716ef8d6c59799c5405b04378368cd241aa796d9
Yogesh Bapu Lokare <yogeshlokare7@gmail.com>
2018-03-08 15:22:34 +0530

    pravin tumsare project permission, role, user controller set and update. committed

---
+++ experience/src/test/java/com/experience/test/TestRoles.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.experience.test;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author Pravin Tumasre
+ *
+ */
+public class TestRoles {
+
+	@Autowired
+	
+	
+	@Test
+	public void test() {
+	}
+
+}
